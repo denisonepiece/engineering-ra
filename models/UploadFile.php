4359a4f7cbab2944cc763bbe500fe67279c42d78
denisonepiece <denisloverap@icloud.com>
2018-11-20 12:06:09 +0700

    Update UploadFile.php

--- models/UploadFile.php
+++ models/UploadFile.php
@@ -5,29 +5,34 @@ namespace app\models;
 use yii\base\Model;
 use yii\web\UploadedFile;
 
-class UploadFile extends Model{
+class UploadFile extends Model
+{
 
- public $img;
+    public $img;
+
     public function attributeLabels()
     {
         return [
             'img' => 'Изображение записи',
         ];
     }
-     public function rules(){
-     return[
+
+    public function rules()
+    {
+        return [
             [['img'], 'file'],
         ];
- }
+    }
 
- public function upload(){
-if($this->validate() && $this->img!==null){
-    $this->img->name = rand(1,9999)."_".date("U").".".$this->img->extension;
-    $this->img->saveAs("uploads/{$this->img->name}");
-return true;
-}else{
-return false;
-}
-}
+    public function upload()
+    {
+        if ($this->validate() && $this->img !== null) {
+            $this->img->name = rand(1, 9999) . "_" . date("U") . "." . $this->img->extension;
+            $this->img->saveAs("uploads/{$this->img->name}");
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
