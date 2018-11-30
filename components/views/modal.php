b9e97ed0d695d2b053136a7a75c6012a045ee045
denisonepiece <denisloverap@icloud.com>
2018-11-30 13:43:28 +0700

    Added reglament in form

--- components/views/modal.php
+++ components/views/modal.php
@@ -7,36 +7,44 @@ use yii\helpers\Html;
 ?>
 
 <!-- Модальное окно из виджета START -->
-    <div class="modal_div" id="modal2">
-      <div class="modal-container__modal-dialog">
+<div class="modal_div" id="modal2">
+    <div class="modal-container__modal-dialog">
         <div class="modal-dialog__modal-header">
-          <!-- Кнопка закрытия. Чтобы закрыть верни modal-visible из родителя -->
-          <a class="modal_close">
-            <svg viewBox="0 0 64 64"><g><path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/></g></svg>
-          </a>
-          <h1 class="header-block">Что Вам требуется?</h1>
+            <!-- Кнопка закрытия. Чтобы закрыть верни modal-visible из родителя -->
+            <a class="modal_close">
+                <svg viewBox="0 0 64 64">
+                    <g>
+                        <path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/>
+                    </g>
+                </svg>
+            </a>
+            <h1 class="header-block">Что Вам требуется?</h1>
         </div>
         <div class="modal-dialog__modal-body">
-          <div class="btn-group btmM">
-            <a href="#modal3" class="btn one-btn open_modal btn-maxW">Проконсультрироваться</a>
-          </div>
-          <div class="btn-group">
-            <a href="#modal4" class="btn one-btn open_modal btn-maxW">Получить услугу</a>
-          </div>
+            <div class="btn-group btmM">
+                <a href="#modal3" class="btn one-btn open_modal btn-maxW">Проконсультрироваться</a>
+            </div>
+            <div class="btn-group">
+                <a href="#modal4" class="btn one-btn open_modal btn-maxW">Получить услугу</a>
+            </div>
         </div>
-      </div>
     </div>
-    <div id="overlay"></div>
-    <!-- Модальное окно END -->
-    <!-- Модальное окно из виджета КОНСУЛЬТАЦИЯ START -->
-    <div class="modal_div scroll" id="modal3">
-      <div class="modal-container__modal-dialog scroll">
+</div>
+<div id="overlay"></div>
+<!-- Модальное окно END -->
+<!-- Модальное окно из виджета КОНСУЛЬТАЦИЯ START -->
+<div class="modal_div scroll" id="modal3">
+    <div class="modal-container__modal-dialog scroll">
         <div class="modal-dialog__modal-header">
-          <!-- Кнопка закрытия. Чтобы закрыть верни modal-visible из родителя -->
-          <a class="modal_close">
-            <svg viewBox="0 0 64 64"><g><path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/></g></svg>
-          </a>
-          <h1 class="header-block">Получение консультации</h1>
+            <!-- Кнопка закрытия. Чтобы закрыть верни modal-visible из родителя -->
+            <a class="modal_close">
+                <svg viewBox="0 0 64 64">
+                    <g>
+                        <path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/>
+                    </g>
+                </svg>
+            </a>
+            <h1 class="header-block">Получение консультации</h1>
         </div>
         <div class="modal-dialog__modal-body">
 
@@ -58,10 +66,10 @@ use yii\helpers\Html;
             ]) ?>
             <?= $form->field($model, 'contact', [
                 'template' => '{label}{input}{error}',
-            ])->textInput()->input('contact',['placeholder' => 'Телефон, e-mail']) ?>
+            ])->textInput()->input('contact', ['placeholder' => 'Телефон, e-mail']) ?>
             <?= $form->field($model, 'data', [
                 'template' => '{label}{input}{error}'
-            ])->textInput()->input('data',['placeholder' => 'Наименование или ИНН']) ?>
+            ])->textInput()->input('data', ['placeholder' => 'Наименование или ИНН']) ?>
 
             <?= $form->field($model, 'subject', [
                 'template' => '{label}{input}{error}'
@@ -70,8 +78,8 @@ use yii\helpers\Html;
             <?= $form->field($model, 'text_subject', [
                 'template' => '{label}{input}{error}'
             ])->textarea([
-                    'cols' => '30',
-                    'rows' => '10',
+                'cols' => '30',
+                'rows' => '10',
             ]) ?>
 
             <div class="form-group">
@@ -80,23 +88,28 @@ use yii\helpers\Html;
 
             <div class="msg-forForm">
                 Нажимая кнопку «Подать заявку», я соглашаюсь с
-                <a href="http://engineering-ra.ru/web/politic.pdf" target="_blank" class="more-link">правилами передачи и обработки персональных данных</a>
+                <a href="http://engineering-ra.ru/web/politic.pdf" target="_blank" class="more-link">правилами передачи
+                    и обработки персональных данных</a>
             </div>
 
             <?php ActiveForm::end(); ?>
         </div>
-      </div>
     </div>
-    <div id="overlay"></div>
-    <!-- Модальное окно END -->
-    <!-- Модальное окно из виджета УСЛУГА START -->
-    <div class="modal_div scroll" id="modal4">
-      <div class="modal-container__modal-dialog scroll">
+</div>
+<div id="overlay"></div>
+<!-- Модальное окно END -->
+<!-- Модальное окно из виджета УСЛУГА START -->
+<div class="modal_div scroll" id="modal4">
+    <div class="modal-container__modal-dialog scroll">
         <div class="modal-dialog__modal-header">
-          <a class="modal_close">
-            <svg viewBox="0 0 64 64"><g><path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/></g></svg>
-          </a>
-          <h1 class="header-block">Заявка на услугу</h1>
+            <a class="modal_close">
+                <svg viewBox="0 0 64 64">
+                    <g>
+                        <path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/>
+                    </g>
+                </svg>
+            </a>
+            <h1 class="header-block">Заявка на услугу</h1>
         </div>
         <div class="modal-dialog__modal-body">
             <?php $form = ActiveForm::begin([
@@ -117,10 +130,10 @@ use yii\helpers\Html;
             ]) ?>
             <?= $form->field($service, 'contact', [
                 'template' => '{label}{input}{error}',
-            ])->textInput()->input('contact',['placeholder' => 'Телефон, e-mail']) ?>
+            ])->textInput()->input('contact', ['placeholder' => 'Телефон, e-mail']) ?>
             <?= $form->field($service, 'data', [
                 'template' => '{label}{input}{error}'
-            ])->textInput()->input('data',['placeholder' => 'Наименование или ИНН']) ?>
+            ])->textInput()->input('data', ['placeholder' => 'Наименование или ИНН']) ?>
 
             <?= $form->field($service, 'about', [
                 'template' => '{label}{input}{error}'
@@ -135,13 +148,18 @@ use yii\helpers\Html;
             </div>
 
             <div class="msg-forForm">
+                <a href="/web/reglament.pdf" target="_blank" class="more-link">Регламенты оказания услуг РЦИ РА</a>
+            </div>
+
+            <div class="msg-forForm">
                 Нажимая кнопку «Подать заявку», я соглашаюсь с
-                <a href="http://engineering-ra.ru/web/politic.pdf" target="_blank" class="more-link">правилами передачи и обработки персональных данных</a>
+                <a href="http://engineering-ra.ru/web/politic.pdf" target="_blank" class="more-link">правилами передачи
+                    и обработки персональных данных</a>
             </div>
 
             <?php ActiveForm::end(); ?>
         </div>
-      </div>
     </div>
-    <div id="overlay"></div>
-    <!-- Модальное окно END -->
+</div>
+<div id="overlay"></div>
+<!-- Модальное окно END -->
--- views/site/page.php
+++ views/site/page.php
@@ -1,12 +1,12 @@
 <?php
 
-$this->registerCssFile('@web/css/flexboxgrid.css',['depends' =>'app\assets\ApppAsset']);
-$this->registerCssFile('@web/css/main.css',['depends' =>'app\assets\ApppAsset']);
-$this->registerCssFile('@web/css/about.css',['depends' =>'app\assets\ApppAsset']);
+$this->registerCssFile('@web/css/flexboxgrid.css', ['depends' => 'app\assets\ApppAsset']);
+$this->registerCssFile('@web/css/main.css', ['depends' => 'app\assets\ApppAsset']);
+$this->registerCssFile('@web/css/about.css', ['depends' => 'app\assets\ApppAsset']);
 
-$this->registerCssFile('@web/css/notification.css',['depends' =>'app\assets\ApppAsset']);
-$this->registerJsFile('@web/js/notification.js',['depends' =>'app\assets\ApppAsset']);
-$this->registerJsFile('@web/js/menu.js',['depends' =>'app\assets\ApppAsset']);
+$this->registerCssFile('@web/css/notification.css', ['depends' => 'app\assets\ApppAsset']);
+$this->registerJsFile('@web/js/notification.js', ['depends' => 'app\assets\ApppAsset']);
+$this->registerJsFile('@web/js/menu.js', ['depends' => 'app\assets\ApppAsset']);
 
 $this->title = $page->title;
 ?>
@@ -17,18 +17,14 @@ $this->title = $page->title;
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="news-tile__header">
-                        <h1 class="header-block header-block--bmrg-under"><?=$page->title?></h1>
+                        <h1 class="header-block header-block--bmrg-under"><?= $page->title ?></h1>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-md-9 col-sm-12 col-xs-12">
-                    <div class="base-text">
-                        <?= $page->content ?>
-                    </div>
-                </div>
+                <?= $page->content ?>
             </div>
         </div>
     </section>
