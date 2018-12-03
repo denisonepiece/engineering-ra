391e37b19633d7f5f9d51aaa7138c9c1b7145c46
Sergey Chekonov <hypelab@yandex.ru>
2018-12-03 17:27:56 +0700

    widget

--- views/site/materials.php
+++ views/site/materials.php
@@ -5,9 +5,53 @@ $this->registerJsFile('@web/js/tabs.js', ['depends' => 'app\assets\ApppAsset']);
 $this->registerCssFile('@web/css/notification.css', ['depends' => 'app\assets\ApppAsset']);
 $this->registerJsFile('@web/js/notification.js', ['depends' => 'app\assets\ApppAsset']);
 $this->registerJsFile('@web/js/menu.js', ['depends' => 'app\assets\ApppAsset']);
+$this->registerJsFile('@web/js/checkbox.js', ['depends' => 'app\assets\ApppAsset']);
 
 $this->title = 'Материалы и публикации';
 ?>
+
+<!-- Модальное окно на виджет оценки -->
+<div class="modal_div" id="modal7">
+    <div class="modal-container__modal-dialog scroll">
+        <div class="modal-dialog__modal-header">
+            <a class="modal_close">
+                <svg viewBox="0 0 64 64">
+                    <g>
+                        <path d="M28.941,31.786L0.613,60.114c-0.787,0.787-0.787,2.062,0,2.849c0.393,0.394,0.909,0.59,1.424,0.59 c0.516,0,1.031-0.196,1.424-0.59l28.541-28.541l28.541,28.541c0.394,0.394,0.909,0.59,1.424,0.59c0.515,0,1.031-0.196,1.424-0.59 c0.787-0.787,0.787-2.062,0-2.849L35.064,31.786L63.41,3.438c0.787-0.787,0.787-2.062,0-2.849c-0.787-0.786-2.062-0.786-2.848,0 L32.003,29.15L3.441,0.59c-0.787-0.786-2.061-0.786-2.848,0c-0.787,0.787-0.787,2.062,0,2.849L28.941,31.786z"/>
+                    </g>
+                </svg>
+            </a>
+            <h1 class="header-block">Оцените качество и полноту информации</h1>
+        </div>
+        <div class="modal-dialog__modal-body">
+            <div class="checkbox-custom-v1 for-content-mb-v2">
+                <div class="checkbox-custom-v1__position">
+                    <input type="checkbox" name="checkbox-option" id="checkbox-button-opt-one" class="hide-checkbox" value="Option 1">
+                    <label for="checkbox-button-opt-one">1 балл</label>
+                 </div>
+                 <div class="checkbox-custom-v1__position">
+                    <input type="checkbox" name="checkbox-option" id="checkbox-button-opt-two" class="hide-checkbox" value="Option 2">
+                    <label for="checkbox-button-opt-two">2 балла</label>
+                </div>
+                 <div class="checkbox-custom-v1__position">
+                    <input type="checkbox" name="checkbox-option" id="checkbox-button-opt-three" class="hide-checkbox" value="Option 3">
+                    <label for="checkbox-button-opt-three">3 балла</label>
+                </div>
+                 <div class="checkbox-custom-v1__position">
+                    <input type="checkbox" name="checkbox-option" id="checkbox-button-opt-four" class="hide-checkbox" value="Option 4">
+                    <label for="checkbox-button-opt-four">4 балла</label>
+                </div>
+                 <div class="checkbox-custom-v1__position">
+                    <input type="checkbox" name="checkbox-option" id="checkbox-button-opt-five" class="hide-checkbox" value="Option 5">
+                    <label for="checkbox-button-opt-five">5 баллов</label>
+                </div>                                                
+            </div>
+                <div class=" form-group btn-group">
+                    <a href="#" class="btn one-btn">Отправить</a>
+                </div>                                     
+        </div>
+    </div>
+</div>
 <main class="content-container">
     <section class="main__materials stock-sec-padd first-sec-top-padd">
         <div class="container">
@@ -129,3 +173,4 @@ $this->title = 'Материалы и публикации';
         </div>
     </section>
 </main>
+<a href="#modal7" class="services-wiget v2 open_modal">Оцените качество информации</a>
---
+++ web/js/checkbox.js
@@ -0,0 +1,3 @@
+[].forEach.call( document.querySelectorAll('.hide-checkbox'), function(element) {
+element.style.display = 'none';
+});
